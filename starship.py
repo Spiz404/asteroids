b258ec88d612330cd79efbef9cca046cdcd60731
spiz404 <lorespiz03@gmail.com>
2024-10-04 18:55:31 +0200

    added wrapping

--- starship.py
+++ starship.py
@@ -19,11 +19,23 @@ class Starship:
         x, y = self.centroid
         angle = self.rotation_angle
         vertices = []
+        outside_right = outside_left = outside_top = outside_down = 0
+
         for i in range(3):
             vx = x + self.size * math.cos(angle)
             vy = y + self.size * math.sin(angle)
+            if vx > SCREEN_WIDTH: outside_right += 1
+            if vx < 0: outside_left += 1
+            if vy < 0: outside_top += 1
+            if vy > SCREEN_HEIGHT: outside_down += 1
             vertices.append((vx, vy))
             angle += 2 * math.pi / 3
+
+        if outside_right == 3: self.centroid = (0, self.centroid[1])
+        if outside_left == 3: self.centroid = (SCREEN_WIDTH, self.centroid[1])
+        if outside_top == 3: self.centroid = (self.centroid[0], SCREEN_HEIGHT)
+        if outside_down == 3: self.centroid = (self.centroid[0], 0)
+
         self.vertices = vertices
 
     def rotate(self, rotation):
