5f76e09e6d02432e464e911f83be69557378ffe8
spiz404 <lorespiz03@gmail.com>
2024-10-04 14:47:41 +0200

    rotating starship

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+import math
+from starship import Starship
+from constants import DEFAULT_SPEED, SCREEN_WIDTH, SCREEN_HEIGHT
+
+screen = pygame.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
+speed = DEFAULT_SPEED
+starship = Starship(1.0, (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+moving = False
+while True:
+    screen.fill((0, 0, 0))
+    if speed < 0.1:
+        moving = False
+        speed = DEFAULT_SPEED
+    starship.draw(screen)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+            pygame.quit()
+            exit()
+
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        #current_rotation = starship.direction
+        moving = True
+        #starship.move(starship.direction, speed)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        starship.rotate(-0.004)
+    if keys[pygame.K_RIGHT]:
+        starship.rotate(0.004)
+    if moving:
+        speed = math.log(speed, 2)
+        #starship.move(current_rotation, speed)
+    pygame.display.update()
+    pygame.display.flip()
---
+++ starship.py
@@ -0,0 +1,60 @@
+import pygame
+import math
+from constants import STARSHIP_SIZE, SCREEN_WIDTH, SCREEN_HEIGHT
+
+class Starship:
+    def __init__(self, rotation_angle, centroid):
+        self.size = STARSHIP_SIZE
+        self.centroid = centroid
+        self.rotation_angle = -math.pi / 2
+        x, y = self.centroid
+        angle = rotation_angle
+        vertices = []
+        for i in range(3):
+            vx = x + self.size * math.cos(angle)
+            vy = y + self.size * math.sin(angle)
+            vertices.append((vx, vy))
+            angle += 2 * math.pi / 3  # Rotate by 120 degrees (2π/3 radians)
+        self.vertices = vertices
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255, 255, 255), self.vertices)
+
+    def update_vertices(self):
+
+        x, y = self.centroid
+        angle = self.rotation_angle
+        vertices = []
+        for i in range(3):
+            vx = x + self.size * math.cos(angle)
+            vy = y + self.size * math.sin(angle)
+            vertices.append((vx, vy))
+            angle += 2 * math.pi / 3
+        self.vertices = vertices
+
+    def rotate(self, rotation):
+       self.rotation_angle += rotation
+       self.update_vertices()
+
+    def move(self, direction, speed):
+        print("moving")
+        print(self.vertices)
+        vertices_outside = 0
+        for i in range(3):
+            x, y = self.vertices[i]
+            x += speed * math.cos(direction)
+            y += speed * math.sin(direction)
+            if x > SCREEN_WIDTH or y > SCREEN_WIDTH or x < 0 or y < 0:
+                vertices_outside += 1
+            self.vertices[i] = (x, y)
+
+        if vertices_outside == 3:
+            if self.vertices[0][0] > SCREEN_WIDTH:
+                self.vertices[0] = (0, self.vertices[0][1])
+                self.vertices[1] = (0, self.vertices[1][1])
+                self.vertices[2] = (0, self.vertices[2][1])
+            if self.vertices[0][1] > SCREEN_HEIGHT:
+                self.vertices[0] = (self.vertices[0][0], 0)
+                self.vertices[1] = (self.vertices[1][0], 0)
+                self.vertices[2] = (self.vertices[2][0], 0)
+        print(self.vertices)
