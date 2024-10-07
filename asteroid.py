111deade450b4fffbac85a8dbf97130ccb835cea
spiz404 <lorespiz03@gmail.com>
2024-10-07 14:57:08 +0200

    update sizes and speeds of asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,18 +1,18 @@
-from constants import ASTEROIDS_SIZES, SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import ASTEROIDS_SPEEDS, ASTEROIDS_SIZES, SCREEN_HEIGHT, SCREEN_WIDTH
 import pygame
 import math
 
 class Asteroid:
     def __init__(self, position, speed, direction, size):
         self.position = position
-        self.speed = speed
         self.direction = direction
         self.size = ASTEROIDS_SIZES[size] if size < len(ASTEROIDS_SIZES) and size > 0 else ASTEROIDS_SIZES[-1]
+        self.speed = ASTEROIDS_SPEEDS[size]
         self.vertices = []
 
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255, 255, 255), self.position, self.size, 1)
+        pygame.draw.circle(screen, (255, 255, 255), self.position, self.size)
 
     def move(self):
         posx = self.position[0] + self.speed * math.cos(self.direction)
